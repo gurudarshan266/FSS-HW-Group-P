5b73b26cb5781814ac35e93019227dc9a933edc1
gurudarshan266 <gpollep@ncsu.edu>
2017-09-12 17:44:09 -0400

    Added workshop answers

---
+++ workshop/gpollep_k20.py
@@ -0,0 +1,57 @@
+# KNN Classification
+# shuffle the data
+np.random.seed(0)
+indices = np.random.permutation(len(iris_X))
+# Split iris data in train and test data
+# A random permutation, to split the data randomly
+iris_X_train = iris_X[indices[:-10]]
+iris_y_train = iris_y[indices[:-10]]
+iris_X_test  = iris_X[indices[-10:]]
+iris_y_test  = iris_y[indices[-10:]]
+
+# Create and fit a nearest-neighbor classifier
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(iris_X_train, iris_y_train) 
+
+# Do prediction on the test data
+knn.predict(iris_X_test)
+print iris_y_test
+
+
+
+# KNN Visualization.
+n_neighbors = 20
+h = .02  # step size in the mesh
+
+# Create color maps
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for n_neighbors in [5,10,15,20,25]:
+    for weights in ['uniform', 'distance']:
+        # we create an instance of Neighbours Classifier and fit the data.
+        knn = KNeighborsClassifier(n_neighbors, weights=weights)
+        knn.fit(X, y)
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+        Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.figure()
+        plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+        # Plot also the training points
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                    edgecolor='k', s=20)
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.title("3-Class classification (k = %i, weights = '%s')"
+                  % (n_neighbors, weights))
+
+plt.show()
