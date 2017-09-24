0aa5b087ca1718afcc8b9817918e2715231d4c0c
Guru Darshan <gpollep@ncsu.edu>
2017-09-24 19:40:27 -0400

    Modified automation script to generate random number of samples

--- HW3/run.sh
+++ HW3/run.sh
@@ -2,9 +2,9 @@
 
 for i in {1..20}
 do
-	
+	N=`expr $RANDOM % 10000`
 	echo -e "\nRUN = #$i\n"
-	python Udiscrete_gen.py
+	python Udiscrete_gen.py $N
 	python SuperDiscrete.py UD_sample.txt
 	echo -e "-------------------------------------------------------------------------------------------------------------------\n\n"
 done
