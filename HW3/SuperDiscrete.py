8af2b525c337745650253314ad920c7a82cb660e
Guru Darshan <gpollep@ncsu.edu>
2017-09-24 18:27:10 -0400

    Fixed integer division issue in Super Discrete

--- HW3/SuperDiscrete.py
+++ HW3/SuperDiscrete.py
@@ -12,7 +12,7 @@ def SupervisedDiscrete(things,x=None,y=None,nump=True,lessp=True):
     y = y or (lambda p:p[-1])
     x = x or (lambda p:p[0])
     better = lessp and (lambda p,q:  p<q) or (lambda p,q:  p>q)
-    what = nump and NUM or SYM
+    #what = nump and NUM or SYM
     compute_spread = nump and (lambda num: num.sd) or (lambda sym: sym.entropy())
     break_points = {}
     ranges = UDiscrete.UDiscretize(things,x)
@@ -22,7 +22,7 @@ def SupervisedDiscrete(things,x=None,y=None,nump=True,lessp=True):
         return ranges[j].arr
 
     def memo(here,stop, _memo, b4=None, inc=None):
-        b4 = b4 or NUM()
+        b4 = NUM()
         inc = 1 if stop > here else -1
         if here != stop:
             b4 = copy.deepcopy(memo(here+inc,stop, _memo))
@@ -46,7 +46,7 @@ def SupervisedDiscrete(things,x=None,y=None,nump=True,lessp=True):
             l = lmemo[j]
             r = rmemo[j+1]
 
-            tmp = l.n/all.n*compute_spread(l) + r.n/all.n*compute_spread(r)
+            tmp = (l.n*1.0/all.n)*compute_spread(l) + (r.n*1.0/all.n)*compute_spread(r)
             if better(tmp, best):
                 cut = j
                 best = tmp
@@ -58,7 +58,7 @@ def SupervisedDiscrete(things,x=None,y=None,nump=True,lessp=True):
             bin_ = combine(cut + 1, high, rbest, bin_, lvl + 1)
         else:
             if bin_ not in break_points:
-                break_points[bin_] = -1e-32
+                break_points[bin_] = -1e32
             if ranges[high].num.max > break_points[bin_]:
                 break_points[bin_] = ranges[high].num.max
 
@@ -67,6 +67,9 @@ def SupervisedDiscrete(things,x=None,y=None,nump=True,lessp=True):
     combine(1, len(ranges)-1, \
             memo(1,len(ranges)-1,{}),\
             1, 0)
+    if len(break_points) < len(ranges)-1:
+        print "\n\nFewer ranges by Supervised Discretizer\n\n"
+
     return break_points
 
 
@@ -91,6 +94,7 @@ if __name__ == '__main__':
     print "\nSupervised Discretizer:"
     for k in breaks:
         print "super\t%s\t{ label = %s, most = %f}"%(str(k),str(k),breaks[k])
+
 
 
 
