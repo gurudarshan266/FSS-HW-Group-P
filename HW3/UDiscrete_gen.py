fd715cf5c38a5cfc3f671355e309b5d60d9d37f9
Guru Darshan <gpollep@ncsu.edu>
2017-09-24 21:05:41 -0400

    Modified generator to support SYM class

--- HW3/UDiscrete_gen.py
+++ HW3/UDiscrete_gen.py
@@ -12,14 +12,27 @@ def klass(x):
     else:
         return 0.9 + random.random()/N
 
+def klass2(x):
+    global N
+    if x < 0.2:
+        return "CAT"
+    elif x < 0.6:
+        return "DOG"
+    else:
+        return "WOLF"
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         N = int(sys.argv[1])
     print "#Samples = %d"%N
 
+    gen_sym = len(sys.argv) > 2 and (sys.argv[2]=='SYM' or sys.argv[2]=='Sym' or sys.argv[2]=='sym')
+
+    klass_func = klass if not gen_sym else klass2
+
     #random.seed(random.randint(0,1000))
     with open("UD_sample.txt","w+") as fp:
         for i in range(N):
             x = random.random()
-            y = klass(x)
-            fp.write("%f %f\n"%(x,y))
+            y = klass_func(x)
+            fp.write("%f %s\n"%(x,y))
