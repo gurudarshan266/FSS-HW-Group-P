ed3bf369a272cdb3a36dd346e56a26899cf44a71
Guru Darshan <gpollep@ncsu.edu>
2017-11-18 13:38:49 -0500

    Added support for ne datasets in preprocess

--- Project/code/preprocess.py
+++ Project/code/preprocess.py
@@ -11,22 +11,45 @@ def preprocess(dataset='lucene', do_smote = True):
     data = {}
     X = {}
     Y = {}
-    if dataset == 'lucene':
-        file = { 'train':base_dir+'lucene-2.0.csv', 'tune':base_dir+'lucene-2.2.csv', 'test':base_dir+'lucene-2.4.csv'}
-        for t in dataset_types:
-            data[t] = pd.read_csv(file[t])
 
-            # Extract features
-            X[t] = data[t].loc[:, 'wmc':'avg_cc']
+    # Map the datasets to training, tuning and testing datasets
+    filename = {}
 
-            # Extract bugs and classify bugs as only binary (0 or 1)
-            Y[t] = data[t].bug.apply(bug_classify)
+    filename['ivy'] = {'train':'ivy-1.1.csv', 'tune':'ivy-1.4.csv', 'test':'ivy-2.0.csv'}
 
-        # SMOTE the training data
-        if do_smote:
-            sm = SMOTE(random_state=1516)
-            X['train'], Y['train'] = sm.fit_sample(X['train'], Y['train'])
+    filename['jedit1'] = {'train': 'jedit-3.2.csv', 'tune': 'jedit-4.0.csv', 'test': 'jedit-4.1.csv'}
+    filename['jedit2'] = {'train': 'jedit-4.0.csv', 'tune': 'jedit-4.1.csv', 'test': 'jedit-4.2.csv'}
+    filename['jedit3'] = {'train': 'jedit-4.1.csv', 'tune': 'jedit-4.2.csv', 'test': 'jedit-4.3.csv'}
 
-        return (X['train'],Y['train'],
-                X['tune'],Y['tune'],
-                X['test'], Y['test'])
+    filename['lucene'] = {'train':'lucene-2.0.csv', 'tune':'lucene-2.2.csv', 'test':'lucene-2.4.csv'}
+
+    filename['velocity'] = {'train': 'velocity-1.4.csv', 'tune': 'velocity-1.5.csv', 'test': 'velocity-1.6.csv'}
+
+    filename['xalan1'] = {'train': 'xalan-2.4.csv', 'tune': 'xalan-2.5.csv', 'test': 'xalan-2.6.csv'}
+    filename['xalan2'] = {'train': 'xalan-2.5.csv', 'tune': 'xalan-2.6.csv', 'test': 'xalan-2.7.csv'}
+
+    filename['xerces'] = {'train':'xerces-1.2.csv', 'tune':'xerces-1.3.csv', 'test':'xerces-1.4.csv'}
+
+
+    # Add base dirs for the file names
+    file = { 'train':base_dir+filename[dataset]['train'], 'tune':base_dir+filename[dataset]['tune'], 'test':base_dir+filename[dataset]['test']}
+    for t in dataset_types:
+        data[t] = pd.read_csv(file[t])
+
+        # Extract features
+        X[t] = data[t].loc[:, 'wmc':'avg_cc']
+
+        # Extract bugs and classify bugs as only binary (0 or 1)
+        Y[t] = data[t].bug.apply(bug_classify)
+
+    # SMOTE the training data
+    if do_smote:
+        sm = SMOTE(random_state=1516)
+        X['train'], Y['train'] = sm.fit_sample(X['train'], Y['train'])
+
+    return (X['train'],Y['train'],
+            X['tune'],Y['tune'],
+            X['test'], Y['test'])
+
+if __name__ == '__main__':
+    pass
