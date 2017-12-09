c623525136cadde970cac55bfec03ac731ee9e91
Guru Darshan <gpollep@ncsu.edu>
2017-12-08 19:59:03 -0500

    Added script for datasets

---
+++ Project/code/getDataset.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+mkdir -p ../datasets/
+cd ../datasets/
+
+#Ivy
+wget "https://zenodo.org/record/322436/files/ivy-1.4.csv"
+wget "https://zenodo.org/record/322436/files/ivy-1.1.csv"
+wget "https://zenodo.org/record/322436/files/ivy-2.0.csv"
+
+#Jedit
+wget "https://zenodo.org/record/268458/files/jedit-3.2.csv"
+wget "https://zenodo.org/record/268458/files/jedit-4.0.csv"
+wget "https://zenodo.org/record/268458/files/jedit-4.1.csv"
+wget "https://zenodo.org/record/268458/files/jedit-4.2.csv"
+wget "https://zenodo.org/record/268458/files/jedit-4.3.csv"
+
+#Lucene
+wget "https://zenodo.org/record/322458/files/lucene-2.0.csv"
+wget "https://zenodo.org/record/322458/files/lucene-2.2.csv"
+wget "https://zenodo.org/record/322458/files/lucene-2.4.csv"
+
+#Xalan
+wget "https://zenodo.org/record/268436/files/xalan-2.4.csv"
+wget "https://zenodo.org/record/268436/files/xalan-2.5.csv"
+wget "https://zenodo.org/record/268436/files/xalan-2.6.csv"
+wget "https://zenodo.org/record/268436/files/xalan-2.7.csv"
+
+#Xerces
+wget "https://zenodo.org/record/268474/files/xerces-1.2.csv"
+wget "https://zenodo.org/record/268474/files/xerces-1.3.csv"
+wget "https://zenodo.org/record/268474/files/xerces-1.4.csv"
+
+# Velocity
+wget "https://zenodo.org/record/322455/files/velocity-1.4.csv" 
+wget "https://zenodo.org/record/322455/files/velocity-1.5.csv"
+wget "https://zenodo.org/record/322455/files/velocity-1.6.csv"
