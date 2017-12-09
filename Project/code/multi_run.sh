1cb3ad1d797ae83462f3297b90b04f63f81805fb
Guru Darshan <gpollep@ncsu.edu>
2017-12-08 22:47:23 -0500

    Fixed bug in multi_run

--- Project/code/multi_run.sh
+++ Project/code/multi_run.sh
@@ -4,10 +4,5 @@ datasets=( ivy jedit1 jedit2 jedit3 lucene velocity xalan1 xalan2 xerces )
 
 for f in "${datasets[@]}"
 do
-    python3 run.py $f f1&> ../output/$f.out && echo "Completed $f..." &
+    python3 run.py $f f1 &> ../output/$f.out && echo "Completed $f..." &
 done
-
-# python3 run.py ivy > ../output/ivy.out && echo "Completed with" &
-# python3 run.py jedit1 > ../output/jedit1.out &
-# python3 run.py jedit2  > ../output/jedit2.out &
-# python3 run.py lucene  > ../output/lucene.out &
